b61bfe99df337fa411cab26f0dd2b5d869e66899
= <=>
2019-09-04 15:20:05 +0200

    Add css to the snake

--- mysnake/snakegame/urls.py
+++ mysnake/snakegame/urls.py
@@ -6,5 +6,6 @@ app_name = 'snakegame'
 urlpatterns = [
     path('', views.index, name='index'),
     path('newgame/', views.new_game, name='newgame'),
-    path('game/', views.game, name='game')
+    path('game/', views.game, name='game'),
+    path('the_end/', views.game, name='the_end')
 ]
--- mysnake/snakegame/views.py
+++ mysnake/snakegame/views.py
@@ -10,6 +10,7 @@ def index(request):
     return render(request, 'snakegame/index.html')
 
 def new_game(request):
+    snake.reset_game()
     snake.create_new_board()
     board = snake.Snake.board
     return render(request, 'snakegame/game.html', {
@@ -17,15 +18,15 @@ def new_game(request):
     })
 
 def game(request):
-    print('req', request)
     move = request.POST.get('move_key')
-    print(move)
     if move == 'w' or move == 's' or move =='a' or move == 'd':
         snake.Snake.currentMove =  move.upper()
     snake.random_treats()
     ans = snake.next_move()
     if (ans == 'The end'):
-        return HttpResponse(ans)
+        return render(request, 'snakegame/the_end.html',{
+            'score' : len(snake.Snake.snakeBody)
+        })
     return render(request, 'snakegame/game.html',{
         'board' : snake.Snake.board
     })
